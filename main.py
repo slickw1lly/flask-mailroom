969f631df18b1dcdf568c01910b28923858bf93c
Kim Truong <kt.kimtruong@gmail.com>
2018-10-06 10:36:21 -0700

    Add create new donation and navigation functionality.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -18,6 +18,16 @@ def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
 
+
+@app.route('/add/', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        donor = Donor.select().where(Donor.name == request.form['name']).get()
+        donation = Donation(value=request.form['amount'], donor=donor)
+        donation.save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('add.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
