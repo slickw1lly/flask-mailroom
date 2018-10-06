1e462405bc076cf61fc730f87ad4ae55744c82c1
Kim Truong <kt.kimtruong@gmail.com>
2018-10-05 23:18:14 -0700

    Initial Commit.

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
